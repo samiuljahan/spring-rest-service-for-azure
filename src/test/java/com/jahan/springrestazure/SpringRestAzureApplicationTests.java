83a2d30ac1c677fa2f383e6e024940b4a66b8a06
samiuljahan <samiuljahan@gmail.com>
2022-10-25 13:22:53 -0500

    sample-rest-service-on-azure

---
+++ src/test/java/com/jahan/springrestazure/SpringRestAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jahan.springrestazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
